997f2aa92438ecbaa263622279bf644057f81875
Jessica Hille <jessicalhille@gmail.com>
2022-01-08 19:46:55 -0600

    functional save and delete buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 var fs = require("fs");
-const { stringify } = require("querystring");
 
 // uuid npm package installed in order to create a unique id for each note
 const { v4: uuidv4} = require('uuid');
@@ -8,13 +7,12 @@ const { v4: uuidv4} = require('uuid');
 // created to post data to db.json
 const db = require('../db/db.json');
 
-// api get route
+// GET method to return all notes
 router.get("/notes", (req, res) => {
-    // send data in response to 'GET' request
     res.json(db);
 });
 
-// api post route
+// POST method to add notes
 router.post("/notes", (req, res) => {
     // pull the newNote from request
     const newNote = req.body;
@@ -22,8 +20,25 @@ router.post("/notes", (req, res) => {
     newNote.id = uuidv4();
     // push the newNote into the db file
     db.push(newNote);
-    // send the response to the db file
+    
     res.json(db);
 });
 
+// DELETE method to delete note by unique id
+router.delete("/notes/:id", (req, res) => {
+    // obtains id and converts to a string
+    const noteId = req.params.id.toString();
+
+    // goes into array in search of the matching id
+    for (i = 0; i < db.length; i++) {
+        if (db[i].id === noteId) {
+            // removes deleted note from list
+            db.splice(i, 1);
+        }
+    }
+    // sends new array back to server
+    res.json(db);
+});
+
+// export to server
 module.exports = router;
